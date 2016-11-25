cb9c12f0c9f12813450972fa402d5d289d800d98
George Bott <insertlogic@gmail.com>
2016-11-25 07:06:08 +0000

    Moved install location selection into `ApplicationWindow`, removed `InstallLocationDialog`

--- OverParse-NT.Client/ApplicationWindow.xaml.cs
+++ OverParse-NT.Client/ApplicationWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.ComponentModel;
 using System.IO;
+using System.Windows.Threading;
 
 namespace OverParse_NT.Client
 {
@@ -25,31 +26,21 @@ namespace OverParse_NT.Client
     /// </summary>
     public partial class ApplicationWindow : Window
     {
-        private DamageDumpManager _Manager;
-        private CancellationTokenSource _ManagerTokenSource = new CancellationTokenSource();
-        private Task _ManagerTask;
+        private CancellationTokenSource _BackgroundRunnerTokenSource = new CancellationTokenSource();
+        private Task _BackgroundRunnerTask;
 
-        public ApplicationWindow(string pso2InstallLocation)
+        public ApplicationWindow()
         {
             InitializeComponent();
 
-            // temp
-            var logDirectoryPath = Path.Combine(pso2InstallLocation, "damagelogs");
-            if (!Directory.Exists(logDirectoryPath))
-                throw new Exception("Logs directory does not exist"); // TODO
-
-            var logFiles = Directory.GetFiles(logDirectoryPath, "*.csv").ToList();
-            if (logFiles.Count == 0)
-                throw new Exception("No log files found");
-
-            logFiles.Sort();
-            logFiles.Reverse();
-            //
-
-            _Manager = new DamageDumpManager(logFiles.First());
-            _Manager.DataChanged += (sender, args) => _UpdateDisplayList(args);
-
-            _ManagerTask = _Manager.Run(_ManagerTokenSource.Token);
+            // Error logging hack
+            void handleException(Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Application closed due to exception");
+                Application.Current.Shutdown();
+            };
+            Dispatcher.UnhandledException += (_, e) => handleException(e.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (_, e) => handleException(e.ExceptionObject as Exception);
         }
 
         private void _UpdateDisplayList(DamageDumpManager.DataChangedEventArgs args)
@@ -72,13 +63,61 @@ namespace OverParse_NT.Client
             });
         }
 
+        private async Task _BackgroundRunner(CancellationToken ct, string installLocation)
+        {
+            // temp
+            var logDirectoryPath = Path.Combine(installLocation, "damagelogs");
+            if (!Directory.Exists(logDirectoryPath))
+                throw new Exception("Logs directory does not exist"); // TODO
+
+            var logFiles = Directory.GetFiles(logDirectoryPath, "*.csv").ToList();
+            if (logFiles.Count == 0)
+                throw new Exception("No log files found");
+
+            logFiles.Sort();
+            logFiles.Reverse();
+            //
+
+            var manager = new DamageDumpManager(logFiles.First());
+            manager.DataChanged += (sender, args) => _UpdateDisplayList(args);
+
+            await manager.Run(ct);
+        }
+
         protected async override void OnClosing(CancelEventArgs e)
         {
             base.OnClosing(e);
 
-            _ManagerTokenSource.Cancel();
-            if (_ManagerTask != null)
-                await _ManagerTask.ContinueWith((_) => { });
+            _BackgroundRunnerTokenSource.Cancel();
+            if (_BackgroundRunnerTask != null)
+                await _BackgroundRunnerTask;
+        }
+
+        private void _InstallSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: non WinForms solution
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Select your `pso2_bin` folder, this will be in the location you installed PSO2";
+            var result = dialog.ShowDialog();
+            if (result != System.Windows.Forms.DialogResult.OK)
+                throw new Exception("DialogResult was not OK");
+
+            bool isValidInstallPath(string path)
+            {
+                if (!Directory.Exists(path))
+                    return false;
+
+                if (Directory.GetFiles(path, "pso2.exe").Length == 0)
+                    return false;
+
+                return true; // more checks I guess
+            }
+
+            if (!isValidInstallPath(dialog.SelectedPath))
+                throw new Exception("Path selected was not valid PSO2 install");
+
+            _BackgroundRunnerTask = _BackgroundRunner(_BackgroundRunnerTokenSource.Token, dialog.SelectedPath);
+            _InstallSelectWrapper.Visibility = Visibility.Collapsed;
         }
     }
 }
